d147ee76e13c1eb9a5f3edd2961cc3ade5f2ea74
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-06-26 21:47:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // Your code here.
+const cubes = document.querySelectorAll('.cube');
+let selectedCube = null;
+let offsetX, offsetY;
+
+cubes.forEach(cube => {
+	cube.addEventListener('mousedown', (e) => {
+		selectedCube = cube;
+		offsetX = e.clientX - cube.getBoundingClientRect().left;
+		offsetX = e.clientY - cube.getBoundingClientRect().top;
+	})
+});
